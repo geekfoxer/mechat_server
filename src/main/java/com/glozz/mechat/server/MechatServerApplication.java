42e7a7615c625c477a897b092967484bbbc37682
zhuji <jing.zhu@yooli.com>
2019-07-17 08:23:09 +0800

    init

---
+++ src/main/java/com/glozz/mechat/server/MechatServerApplication.java
@@ -0,0 +1,13 @@
+package com.glozz.mechat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glozz/mechat/server/MechatServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glozz.mechat.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MechatServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
